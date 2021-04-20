aa492d7f9835646d6e06a16833e09c2c99a55fb5
zhouzhihu <zzhou998@163.com>
2021-04-20 22:35:33 +0800

    1.升级 gulp 为 4.0 版本 2.修改 gulp 配置文件

--- build/build-dev.js
+++ build/build-dev.js
@@ -3,42 +3,66 @@ const less = require("gulp-less");
 const cssmin = require("gulp-clean-css");
 const rename = require("gulp-rename");
 
-gulp.task("compile-css", () => {
-    return gulp
-        .src(["../src/**/*.less", "!../src/**/_*.less"])
-        .pipe(less())
-        .pipe(cssmin())
-        .pipe(
-            rename((path) => {
-                path.extname = ".wxss";
-            })
-        )
-        .pipe(gulp.dest("../examples/dist/"));
-});
-
-gulp.task("compile-js", () => {
-    return gulp.src(["../src/**/*.js"]).pipe(gulp.dest("../examples/dist/"));
-});
-
-gulp.task("compile-json", () => {
-    return gulp.src(["../src/**/*.json"]).pipe(gulp.dest("../examples/dist/"));
-});
-
-gulp.task("compile-wxml", () => {
-    return gulp.src(["../src/**/*.wxml"]).pipe(gulp.dest("../examples/dist/"));
-});
-
-gulp.task("auto", () => {
-    gulp.watch("../src/**/*.less", ["compile-css"]);
-    gulp.watch("../src/**/*.js", ["compile-js"]);
-    gulp.watch("../src/**/*.json", ["compile-json"]);
-    gulp.watch("../src/**/*.wxml", ["compile-wxml"]);
-});
-
-gulp.task("default", [
+gulp.task(
     "compile-css",
+    gulp.series(() => {
+        return gulp
+            .src(["../src/**/*.less", "!../src/**/_*.less"])
+            .pipe(less())
+            .pipe(cssmin())
+            .pipe(
+                rename((path) => {
+                    path.extname = ".wxss";
+                })
+            )
+            .pipe(gulp.dest("../examples/dist/"));
+    })
+);
+
+gulp.task(
     "compile-js",
+    gulp.series(() => {
+        return gulp
+            .src(["../src/**/*.js"])
+            .pipe(gulp.dest("../examples/dist/"));
+    })
+);
+
+gulp.task(
     "compile-json",
+    gulp.series(() => {
+        return gulp
+            .src(["../src/**/*.json"])
+            .pipe(gulp.dest("../examples/dist/"));
+    })
+);
+
+gulp.task(
     "compile-wxml",
+    gulp.series(() => {
+        return gulp
+            .src(["../src/**/*.wxml"])
+            .pipe(gulp.dest("../examples/dist/"));
+    })
+);
+
+gulp.task(
     "auto",
-]);
+    gulp.series(() => {
+        gulp.watch("../src/**/*.less", gulp.series("compile-css"));
+        gulp.watch("../src/**/*.js", gulp.series("compile-js"));
+        gulp.watch("../src/**/*.json", gulp.series("compile-json"));
+        gulp.watch("../src/**/*.wxml", gulp.series("compile-wxml"));
+    })
+);
+
+gulp.task(
+    "default",
+    gulp.series(
+        "compile-css",
+        "compile-js",
+        "compile-json",
+        "compile-wxml",
+        "auto"
+    )
+);
--- build/build-prod.js
+++ build/build-prod.js
@@ -3,22 +3,28 @@ const less = require("gulp-less");
 const cssmin = require("gulp-clean-css");
 const rename = require("gulp-rename");
 
-gulp.task("compile-css", () => {
-    return gulp
-        .src(["../src/**/*.less", "!../src/**/_*.less"])
-        .pipe(less())
-        .pipe(cssmin())
-        .pipe(
-            rename((path) => {
-                path.extname = ".wxss";
-            })
-        )
-        .pipe(gulp.dest("../dist/"));
-});
+gulp.task(
+    "compile-css",
+    gulp.series(() => {
+        return gulp
+            .src(["../src/**/*.less", "!../src/**/_*.less"])
+            .pipe(less())
+            .pipe(cssmin())
+            .pipe(
+                rename((path) => {
+                    path.extname = ".wxss";
+                })
+            )
+            .pipe(gulp.dest("../dist/"));
+    })
+);
 
-gulp.task("compile-js", () => {
-    return gulp.src(["../src/**/*.js"]).pipe(gulp.dest("../dist/"));
-});
+gulp.task(
+    "compile-js",
+    gulp.series(() => {
+        return gulp.src(["../src/**/*.js"]).pipe(gulp.dest("../dist/"));
+    })
+);
 
 gulp.task("compile-json", () => {
     return gulp.src(["../src/**/*.json"]).pipe(gulp.dest("../dist/"));
@@ -28,9 +34,7 @@ gulp.task("compile-wxml", () => {
     return gulp.src(["../src/**/*.wxml"]).pipe(gulp.dest("../dist/"));
 });
 
-gulp.task("default", [
-    "compile-css",
-    "compile-js",
-    "compile-json",
-    "compile-wxml",
-]);
+gulp.task(
+    "default",
+    gulp.series("compile-css", "compile-js", "compile-json", "compile-wxml")
+);
